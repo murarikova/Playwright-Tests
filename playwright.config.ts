1609a27f72988d76121fbbc28e40a4974d0be743
murarikova <uhercikova.andrea@gmail.com>
2024-03-08 12:35:20 +0100

    upraveny config.ts file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices} from '@playwright/test';
+import { testPlanFilter } from "allure-playwright/dist/testplan";
+
+
 
 /**
  * Read environment variables from file.
@@ -10,6 +13,10 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+
+  grep: testPlanFilter(),
+  reporter: [["line"],["allure playwright"]],
+
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -20,7 +27,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -74,4 +81,6 @@ export default defineConfig({
   //   url: 'http://127.0.0.1:3000',
   //   reuseExistingServer: !process.env.CI,
   // },
+
 });
+
